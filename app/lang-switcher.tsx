ed144934b38fd399d91ff0d8638086c38fa0b8a4
Dereje <95926729+Dereje-getu-tadesse-10@users.noreply.github.com>
2024-02-02 12:12:20 +0100

    update: create header app

---
+++ app/lang-switcher.tsx
@@ -0,0 +1,38 @@
+"use client";
+import { useChangeLocale, useCurrentLocale } from "@/locales/client";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+
+export const LangSwitcher = () => {
+  const changeLocale = useChangeLocale();
+  const locale = useCurrentLocale();
+  console.log(locale);
+  return (
+    <Select
+      onValueChange={(value: "fr" | "en") => {
+        changeLocale(value);
+      }}
+    >
+      <SelectTrigger className="w-[135px]">
+        <SelectValue
+          placeholder={locale === "fr" ? "🇫🇷 French" : "🇬🇧 English"}
+        />
+      </SelectTrigger>
+      <SelectContent>
+        <SelectItem value="fr">
+          <span className="mr-2">🇫🇷</span>
+          French
+        </SelectItem>
+        <SelectItem value="en">
+          <span className="mr-2">🇬🇧</span>
+          English
+        </SelectItem>
+      </SelectContent>
+    </Select>
+  );
+};
