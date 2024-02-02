d324cf6c56045cf8b95304d63a032131c4c98166
Dereje <95926729+Dereje-getu-tadesse-10@users.noreply.github.com>
2024-02-02 12:40:08 +0100

    update: hide locale url

--- middleware.ts
+++ middleware.ts
@@ -1,14 +1,12 @@
 import { createI18nMiddleware } from "next-international/middleware";
-import { NextRequest } from "next/server";
 
 const I18nMiddleware = createI18nMiddleware({
   locales: ["en", "fr"],
-  defaultLocale: "en",
+  defaultLocale: "fr",
+  urlMappingStrategy: "rewrite",
 });
 
-export function middleware(request: NextRequest) {
-  return I18nMiddleware(request);
-}
+export default I18nMiddleware;
 
 export const config = {
   matcher: ["/((?!api|static|.*\\..*|_next|favicon.ico|robots.txt).*)"],
